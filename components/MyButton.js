64b8eb326a4416540d02c61926ec4c54431da96e
graphicalevolution <graphicalevolution@gmail.com>
2022-06-18 16:05:04 +1000

    Added some style

--- components/MyButton.js
+++ components/MyButton.js
@@ -5,8 +5,9 @@ export function MyButton( props ) {
   const[ click, setClick ] = useState(0)  
     return (
          <TouchableOpacity style= { styles.button } onPress= { () => { setClick(click+1) }}>
-            <Text style= { styles.buttonText }> Received {click} clicks </Text>
-            <Text onPress= { () => { setClick(0) }} style= { styles.buttonText }> Reset {click} </Text>
+            <Text style= { styles.buttonText }> Number of click: {click}  </Text>
+            <Text onPress= { () => { setClick(Math.floor(Math.random() * 100) + 1) }} style= { styles.buttonText }> Generate number: {click} </Text>
+            <Text onPress= { () => { setClick(0) }} style= { styles.buttonText } textAlign="center"> Reset </Text>
          </TouchableOpacity>
            
     )  
@@ -28,8 +29,16 @@ const styles =  StyleSheet.create ({
     margin: 5,
   },
   buttonText: {
-    color: "white"
+    color: "white",
+    textAlign: 'center',
     
   }
 
+    // fontWeight: 'bold',
+    // fontSize: 18,
+    // marginTop: 0,
+    // width: 200,
+    // backgroundColor: 'yellow',
 })
+
+
